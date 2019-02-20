cbb447f68b849db128420d5add23b34c81ee76dc
Ali Moallim <axj.159@gmail.com>
2019-02-21 00:07:24 +0900

    Update cuckoo.py

--- cuckoo.py
+++ cuckoo.py
@@ -1,14 +1,38 @@
 
 
-class cuckooFilter():
-	def __init__(self, table_size, bucket_size, finger_print_size, hash_fns):
+class cuckooBucket:
+	def __init__(self, bucket_size):
+		self.bucket_size = bucket_size
+		self.bucket = []
 		pass
 
-	def insert(value):
+	def __contain__(self, finger_print):
+		pass
+		
+	def insert(self):
 		pass
 
-	def delete():
+	def delete(self):
 		pass
 
-	def lookup():
+class cuckooFilter:
+	def __init__(self, table_size, bucket_size, finger_print_size, hash_fns, max_swaps= 100):
+		self.table_size = table_size
+		self.bucket_size = bucket_size
+		self.finger_print_size = finger_print_size
+		self.hash_fns = hash_fns
+		self.max_swaps = max_swaps
+		self.table = []
+
+		for i in range(self.table_size):
+			self.table.append() # TODO: cuckooBucket
+		pass
+
+	def insert(self, value):
+		pass
+
+	def delete(self):
+		pass
+
+	def lookup(self):
 		pass
