d3e9bd0cfea7bca14b5e0946ffe2a6853c8f83e2
Ali Moallim <axj.159@gmail.com>
2019-02-20 23:52:37 +0900

    repo refactored

---
+++ cuckoo.py
@@ -0,0 +1,14 @@
+
+
+class cuckooFilter():
+	def __init__(self, table_size, bucket_size, finger_print_size, hash_fns):
+		pass
+
+	def insert(value):
+		pass
+
+	def delete():
+		pass
+
+	def lookup():
+		pass
